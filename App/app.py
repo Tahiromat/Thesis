008960b6eb6a01ed5c1a0717daf6d1253d37348a
TAHİR MAT <matahir33@gmail.com>
2022-04-28 00:28:54 +0300

    Horizontal app bar created for streamlit

--- App/app.py
+++ App/app.py
@@ -1,11 +1,127 @@
-import streamlit as st
+from tkinter import Y
+import matplotlib
 import pandas as pd
+import streamlit as st
+# import fbprophet import Prophet
+# from pbprophet.plot import Prophet
+from plotly import graph_objs as go
+from streamlit_option_menu import option_menu
 from streamlit_page_helper_methods import city_names
+
+
 
 MAIN_FOLDER_PATH = '/home/tahir/Documents/DataScience/HavaKalitesiAnomaliTespiti/Dataset'
 CITY_NAMES_FOR_SIDE_BAR = city_names(MAIN_FOLDER_PATH)
 
 st.write("""### Anomaly Detection in Air Quality of Turkey """)
-
+st.markdown(""" ### """)
+st.markdown(""" ### """)
 st.sidebar.selectbox('Cities', CITY_NAMES_FOR_SIDE_BAR)
+
+
+# From here you will choose data set 
+df = pd.read_csv('/home/tahir/Documents/DataScience/HavaKalitesiAnomaliTespiti/Dataset/İstanbul/İstanbulAverageDF.csv')
+print(df.columns)
+df = df[:-1]
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S.%f')
+
+
+# Horizontal Menu
+selected = option_menu(None, ["Home", "Visualization", "Forecasting", 'Anomaly D.'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+
+if selected == "Home":
+    def visualize_PM10():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['PM10 ( µg/m3 )']))
+        fig.layout.update(title_text='PM10 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_PM10()
+
+
+if selected == "Visualization":
+    def visualize_PM10():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['PM10 ( µg/m3 )']))
+        fig.layout.update(title_text='PM10 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_PM10()
+
+
+    def visualize_PM_25():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['PM 2.5 ( µg/m3 )']))
+        fig.layout.update(title_text='PM 2.5 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_PM_25()
+
+
+    def visualize_SO2():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['SO2 ( µg/m3 )']))
+        fig.layout.update(title_text='SO2 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_SO2()
+
+
+    def visualize_CO():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['CO ( µg/m3 )']))
+        fig.layout.update(title_text='CO ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_CO()
+
+
+    def visualize_NO2():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['NO2 ( µg/m3 )']))
+        fig.layout.update(title_text='NO2 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_NO2()
+
+
+    def visualize_NOX():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['NOX ( µg/m3 )']))
+        fig.layout.update(title_text='NOX ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_NOX()
+
+
+    def visualize_O3():
+        fig =go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['O3 ( µg/m3 )']))
+        fig.layout.update(title_text='O3 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_O3()
+
+if selected == "Forecasting":
+    def visualize_PM10():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['PM10 ( µg/m3 )']))
+        fig.layout.update(title_text='PM10 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_PM10()
+
+
+if selected == "Anomaly D.":
+    def visualize_PM10():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['Date'], y=df['PM10 ( µg/m3 )']))
+        fig.layout.update(title_text='PM10 ( µg/m3 )', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+    visualize_PM10()
+
+
 
--- DataVisualization/visualization.py
+++ DataVisualization/visualization.py
@@ -18,8 +18,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('/home/tahir/Documents/DataScience/HavaKalitesiAnomaliTespiti/Dataset/İstanbul/İstanbulAverageDF.csv')
+print(df.columns)
 df = df[:-1]
 df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S.%f')
+
+
 
 name_list = df.columns[1:]
 for name in name_list:
@@ -27,5 +30,54 @@ for name in name_list:
     plt.title(name)
     # plt.grid()
     plt.show()
+
+
+
+def visualize_PM10():
+    sns.lineplot(data=df, x='Date', y='PM10 ( µg/m3 )')
+    plt.title('PM10 ( µg/m3 )')
+    plt.show()
+
+def visualize_PM_25():
+    sns.lineplot(data=df, x='Date', y='PM 2.5 ( µg/m3 )')
+    plt.title('PM 2.5 ( µg/m3 )')
+    plt.show()
+
+def visualize_SO2():
+    sns.lineplot(data=df, x='Date', y='SO2 ( µg/m3 )')
+    plt.title('SO2 ( µg/m3 )')
+    plt.show()
+
+def visualize_CO():
+    sns.lineplot(data=df, x='Date', y='CO ( µg/m3 )')
+    plt.title('CO ( µg/m3 )')
+    plt.show()
+
+def visualize_NO2():
+    sns.lineplot(data=df, x='Date', y='NO2 ( µg/m3 )')
+    plt.title('NO2 ( µg/m3 )')
+    plt.show()
+
+def visualize_NOX():
+    sns.lineplot(data=df, x='Date', y='NOX ( µg/m3 )')
+    plt.title('NOX ( µg/m3 )')
+    plt.show()
+
+def visualize_O3():
+    sns.lineplot(data=df, x='Date', y='O3 ( µg/m3 )')
+    plt.title('O3 ( µg/m3 )')
+    plt.show()
+
+
+
+
+visualize_PM10()
+visualize_PM_25()
+visualize_SO2()
+visualize_CO()
+visualize_NO2()
+visualize_NOX()
+visualize_O3()
+
 
 
