61e0442d33d058b2a5a65dcc843d8cf42326bfc6
TAHİR MAT <matahir33@gmail.com>
2022-05-09 14:45:51 +0300

    .

--- App_Pages/visualization_page.py
+++ App_Pages/visualization_page.py
@@ -16,7 +16,7 @@ def visualize_scatter_plot(st, data, y_axis):
     data = data.loc[data.index >= '2022-03-01']
     data = data.resample('D').mean()
     fig = go.Figure(data=[go.Scatter(x=data.index, y=data[y_axis], mode = "markers")])
-    # fig.layout.update(title_text=y_axis, xaxis_rangeslider_visible=True, width=1500, height=600)
+    fig.layout.update(title_text=y_axis, xaxis_rangeslider_visible=True, width=1500, height=600)
     st.plotly_chart(fig)
 
 def visualize_histogram_plot(st, data, y_axis):
