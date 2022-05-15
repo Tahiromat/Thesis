b07ffa7696a328a88f4c25eefc64862404dde522
TAHİR MAT <matahir33@gmail.com>
2022-05-15 14:14:09 +0300

    hist graph updated

--- App_Pages/home_page.py
+++ App_Pages/home_page.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 def home_page(st, data):
     data['Date'] = pd.to_datetime(data['Date'])
+    
     st.write(data.head())
+
 
--- App_Pages/visualization_page.py
+++ App_Pages/visualization_page.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 from plotly import graph_objs as go
+from sqlalchemy import false
 
 def visualize_line_plot(st, data, y_axis):
     data.index = pd.to_datetime(data.index)
@@ -28,9 +30,11 @@ def visualize_area_chart(st, data, y_axis):
 
 def visualize_histogram_plot(st, data, y_axis):
     data.index = pd.to_datetime(data.index)
-    data = data.loc[data.index >= '2022-03-01']
+    data = data.loc[data.index >= '2020-03-01']
     data = data.resample('D').mean()
-    fig = px.histogram(data, x=data[y_axis])
-    fig.layout.update(title_text=y_axis, xaxis_rangeslider_visible=True, width=1500, height=600)
-    st.plotly_chart(fig)
 
+    hist_data = [data[y_axis]]
+    group_labels = [y_axis]
+    fig = ff.create_distplot(hist_data, group_labels, bin_size=.2)
+    fig.layout.update(title_text=y_axis, xaxis_rangeslider_visible=False, width=1500, height=600)
+    st.plotly_chart(fig)
