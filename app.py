607a1674108f72cd32d0c62b5e1b14aa90545a07
TAHİR MAT <matahir33@gmail.com>
2022-05-25 13:19:25 +0300

    .

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ with st.sidebar:
 
 if selected_page == "Home":
     st.write(data_path)
-    data = pd.read_csv(data_path)
+    # data = pd.read_csv(data_path)
     home_page(st, data)
 
 elif selected_page == "Visualization":
